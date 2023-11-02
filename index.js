b2e0bd8973843e1e97c3125aabcbc2e5b637d41d
Jeffrey <jefbfitzp33@outlook.com>
2023-11-01 21:49:38 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(amount){
+  if (amount < 400){
+    return("This one is on me!")
+  } else if  (amount < 2000){
+    return('That will be twenty bucks.')
+  }else if (amount < 2500){
+    return('I will gladly take your thirty bucks.')
+  } else {
+    return('No can do.');
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  
+  const NYC =  city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return NYC;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  const tipped = tip;
+
+  switch(tipped){
+    case 'generous':
+    return 'Thank you so much.'
+    case 'not as generous':
+    return 'Thank you.'
+    default:
+    return 'Bye.'
+
+  }
+
+
 }
